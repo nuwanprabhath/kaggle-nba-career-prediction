258c59e406a398d3435b7f6dbfa60733ba01c746
nuwanprabhath <nuwanprabhath@gmail.com>
2022-02-18 22:22:51 +1100

    Parameter optimization using hpsklearn

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,11 +1,20 @@
 import numpy as np
+from sklearn.metrics import roc_auc_score
 
 
-def show_random_forest_stats(result, expected):
-    """ Showing stats related to the predicted and expected results"""
-    error = abs(result - expected)  # Calculating the error
-    avgAbsSrr = np.mean(error)      # Calculating average absolute error
-    print('Average absolute error: {}%'.format(avgAbsSrr.values[0]*100))
+def show_random_forest_stats(rf, test_X, y_test, rf_probs):
+    """ Showing stats related to the predicted and expected results
+    rf: Trained random forest model
+    test_X: training set
+    y_test: Actual labels 
+    rf_probs: Probability of selected label (in this case 1)
+    """
+
+    # Showing the mean error
+    print('Average absolute error: {}%'.format((1-rf.score(test_X, y_test))*100))
+
+    # Showing the ROC
+    print('ROC: {:0.5f}'.format(roc_auc_score(y_test, rf_probs)))
 
 
 def show_feature_importance(rf, features):
